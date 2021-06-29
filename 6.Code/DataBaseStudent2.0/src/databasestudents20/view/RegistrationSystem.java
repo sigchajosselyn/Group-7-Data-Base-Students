56573a256d4b5cf3f3fba5be16c6b7764fa6f31e
TacoLizeth19 <lctaco@espe.edu.ec>
2021-06-29 01:48:05 -0500

    Change code

--- 6.Code/DataBaseStudent2.0/src/databasestudents20/view/RegistrationSystem.java
+++ 6.Code/DataBaseStudent2.0/src/databasestudents20/view/RegistrationSystem.java
@@ -26,7 +26,7 @@ public class RegistrationSystem {
         do{
             System.out.println("--Students Registration--");
             System.out.println("------------------");
-            System.out.println("1.-Enter ID (Cedula del estudiante)");
+            System.out.println("1.-Enter ID (Cedula)");
             System.out.println("2.-Search Student");
             System.out.println("3.-Exit");
             System.out.println("Select your option....: ");
@@ -47,7 +47,7 @@ public class RegistrationSystem {
                     listarAlumno();
                     break;
                 case 4:
-                    System.out.println("cerrar codigo");
+                    System.out.println("cerrar el codigo");
              
             }
         } while (opcion != 4);
