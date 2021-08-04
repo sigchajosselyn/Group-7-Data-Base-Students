20547c880a3f7e124442d64452671dd5cb0283e4
sigchajosselyn <kossesigcha@gmail.com>
2021-08-03 21:50:43 -0500

    controller

--- 6.Code/DataBaseStudent2.0/src/ec/edu/espe/database/controller/StudentInformationController.java
+++ 6.Code/DataBaseStudent2.0/src/ec/edu/espe/database/controller/StudentInformationController.java
@@ -33,6 +33,32 @@ public void init_database() {
 		db = mongoClient.getDatabase(this.db_name);  
 		table = db.getCollection(this.db_table);  
 	}
+
+public boolean insert_student(Student new_student) {
+		boolean result = false;
+		try {
+			doc = new Document("ci", new_student.getCi());  
+			doc.append("name",new_student.getName());
+			doc.append("last_name",new_student.getLastName());
+			doc.append("civil_status",new_student.getCivilStatus());
+			doc.append("sex",new_student.getSex());
+			doc.append("semester",new_student.getSemester());
+			doc.append("status",new_student.getStatus());
+			doc.append("username",new_student.getUsername());
+			doc.append("email",new_student.getEmail());
+			doc.append("phone",new_student.getPhone());
+			doc.append("password",new_student.getPassword());
+			doc.append("university",new_student.getUniversity());
+			doc.append("subject",new_student.getSubject());
+			doc.append("ncr",new_student.getNCR());
+			doc.append("created_at",new_student.getCreatedAt());  
+			table.insertOne(doc);
+			result = true;
+		} catch (Exception e) {
+			System.out.println("Error to try insert a new element in database");
+		}
+		return result;
+	}
 	
 
         
