027fe228f9aaa9b0d0984784a54684bb0c1c00ab
Gabriela-Sunta <jgsunta@espe.edu.ec>
2021-08-03 21:19:35 -0500

    StudentController

--- 6.Code/DataBaseStudent2.0/src/ec/edu/espe/database/controller/StudentInformationController.java
+++ 6.Code/DataBaseStudent2.0/src/ec/edu/espe/database/controller/StudentInformationController.java
@@ -5,10 +5,21 @@
  */
 package ec.edu.espe.database.controller;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 /**
  *
  * @author Cristopher Sarmiento Futures Programmers ESPE-DCC0
  */
 public class StudentInformationController {
-    
+    MongoClient mongoClient;
+	MongoDatabase db;
+	MongoCollection<Document> table;
+	Document doc;
+	private String db_name = "students_database";
+	private String db_table;
+	
 }
