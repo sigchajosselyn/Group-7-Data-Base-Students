b5f1e04ed6877cae2c4adec79a23922c4e9e0983
TacoLizeth19 <lctaco@espe.edu.ec>
2021-08-04 01:16:14 -0500

    update

--- 6.Code/DataBaseStudent2.0/src/ec/edu/espe/database/view/FindStudent.java
+++ 6.Code/DataBaseStudent2.0/src/ec/edu/espe/database/view/FindStudent.java
@@ -5,8 +5,12 @@
  */
 package ec.edu.espe.database.view;
 import ec.edu.espe.database.model.Student;
+import java.awt.event.ActionListener;
 import java.io.FileWriter;
 import java.io.IOException;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
  *
@@ -14,6 +18,42 @@ import java.io.IOException;
  */
 public class FindStudent {
     
+     public JFrame frame;
+
+	/**
+	 * Create the application.
+	 */
+	public FindStudent() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblFind = new JLabel("FInd");
+		lblFind.setBounds(79, 79, 70, 15);
+		frame.getContentPane().add(lblFind);
+		
+		JButton btnGoBack = new JButton("Go Back");
+		btnGoBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				frame.setVisible(false);
+				new StudentWelcome().frame.setVisible(true);
+			}
+                        
+		});
+		btnGoBack.setBounds(47, 177, 117, 25);
+		frame.getContentPane().add(btnGoBack);
+	}
+
+    
+}
     
     
     
