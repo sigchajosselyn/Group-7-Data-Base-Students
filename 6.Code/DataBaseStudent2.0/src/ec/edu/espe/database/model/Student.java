b21100745e7b14bd3ee2b939e0cdf03c93e771e0
Gabriela-Sunta <jgsunta@espe.edu.ec>
2021-08-03 21:07:58 -0500

    extend

--- 6.Code/DataBaseStudent2.0/src/ec/edu/espe/database/model/Student.java
+++ 6.Code/DataBaseStudent2.0/src/ec/edu/espe/database/model/Student.java
@@ -37,6 +37,218 @@ public class Student extends StudentInformation{
         this.subject = subject;
 		System.out.println("Creating a new student");
 	}
+@Override
+	public String getCi() {
+		return this.ci;
+	}
 
+	@Override
+	public String getName() {
+		return this.name;
+	}
+
+	@Override
+	public String getCivilStatus() {
+		return this.civilStatus;
+	}
+
+	@Override
+	public String getSex() {
+		return this.sex;
+	}
+
+	@Override
+	public String getSemester() {
+		return this.semester;
+	}
+
+//	@Override
+//	public String getNrc() {
+//		return this.nrc;
+//	}
+
+	@Override
+	public String getStatus() {
+		return this.status;
+	}
+
+	@Override
+	public ArrayList<Double> getNotes() {
+		return null;
+	}
+
+	@Override
+	public double getAverage_notes() {
+		return this.average_notes;
+	}
+
+
+	@Override
+	public void setCi(String ci) {
+		this.ci = ci;
+	}
+
+	@Override
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void setCivilStatus(String civilStatus) {
+		this.civilStatus = civilStatus;
+	}
+
+	@Override
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	@Override
+	public void setSemester(String semester) {
+		this.semester = semester;
+	}
+
+//	@Override
+//	public void setNrc(String nrc) {
+//		this.nrc = nrc;
+//	}
+
+	@Override
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public void setNotes(ArrayList<Double> notes) {
+		this.notes = notes;
+	}
+
+	@Override
+	public void setAverage_notes(double average_notes) {
+		this.average_notes = average_notes;		
+	}
+
+	@Override
+	public String getLastName() {
+		return this.last_name;
+	}
+
+	@Override
+	public void setLastName(String last_name) {
+		this.last_name = last_name;
+	}
+
+
+	@Override
+	public String getUsername() {
+		return this.username;
+	}
+
+
+	@Override
+	public String getEmail() {
+		return this.email;
+	}
+
+
+	@Override
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	@Override
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	@Override
+	public String getPhone() {
+		return this.phone;
+	}
+
+
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
+
+	@Override
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+	@Override
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	@Override
+	public String getCreatedAt() {
+		return this.created_at;
+	}
+
+
+	@Override
+	public void  setCreatedAt(String date) {
+		this.created_at = date;
+	}
+
+
+	@Override
+	public String getUniversity() {
+		return this.university;
+	}
+
+
+	@Override
+	public String getNCR() {
+		return this.nrc;
+	}
+
+
+	@Override
+	public void setUniversity(String university) {
+		this.university = university;
+	}
+
+
+	@Override
+	public void setNCR(String ncr) {
+		this.nrc = ncr;
+	}
+
+
+	@Override
+	public String getSubject() {
+		return this.subject;
+	}
+
+
+	@Override
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Student [ci=" + ci + ", name=" + name + ", last_name=" + last_name + ", civilStatus=" + civilStatus
+				+ ", sex=" + sex + ", semester=" + semester + ", status=" + status + ", username=" + username
+				+ ", email=" + email + ", phone=" + phone + ", password=" + password + ", created_at=" + created_at
+				+ ", university=" + university + ", nrc=" + nrc + ", subject=" + subject + ", notes=" + notes
+				+ ", average_notes=" + average_notes + "]";
+	}
+
+   
+    
+    
     
 }
+
+    
+
