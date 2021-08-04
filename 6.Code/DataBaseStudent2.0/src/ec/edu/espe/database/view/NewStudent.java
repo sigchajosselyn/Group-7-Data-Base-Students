be51eb8cfffe5d3aeabd614d02f775c60408369e
sigchajosselyn <kossesigcha@gmail.com>
2021-08-03 23:00:32 -0500

    new

--- 6.Code/DataBaseStudent2.0/src/ec/edu/espe/database/view/NewStudent.java
+++ 6.Code/DataBaseStudent2.0/src/ec/edu/espe/database/view/NewStudent.java
@@ -6,8 +6,12 @@
 package ec.edu.espe.database.view;
 import ec.edu.espe.database.controller.StudentInformationController;
 import ec.edu.espe.database.model.Student;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
@@ -30,6 +34,35 @@ public class NewStudent {
 	public Student student;
 	private String db_table = "students";
     
+        
+        /**
+	 * Create the application.
+	 */
+	public NewStudent() {
+		initialize();
+		db_conection = new StudentInformationController(this.db_table);
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 982, 682);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewStudent = new JLabel("New Student");
+		lblNewStudent.setBounds(360, 22, 154, 15);
+		frame.getContentPane().add(lblNewStudent);
+		
+		JButton btnGoBack = new JButton("Go Back");
+		btnGoBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				frame.setVisible(false);
+				new StudentWelcome().frame.setVisible(true);
+			}
+		});
     
 }
 
